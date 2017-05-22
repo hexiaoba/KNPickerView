704a120e88c72499786485afaeb39e1f5403a329
hexiaoba <hkn35135@163.com>
2017-05-22 17:54:32 +0800

    udpate

---
+++ Carthage/Build/iOS/KNPickerViewCarthage.framework/Headers/KNPickerView.h
@@ -0,0 +1,53 @@
+//
+//  DayPickerView.h
+//  风向标
+//
+//  Created by 何凯楠 on 15/10/27.
+//  Copyright (c) 2015年 何凯楠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class KNPickerView;
+@protocol KNPickerViewDelegate <NSObject>
+
+@optional
+
+- (void)pickerView:(KNPickerView *)pickerView didSelectOneColumResult:(NSString *)OneColumResult twoColumResult:(NSString *)twoColumResult threeColumResult:(NSString *)threeColumResult inComponent:(NSInteger)component;
+
+- (void)pickerView:(KNPickerView *)pickerView getOneCloumResult:(NSString *)oneColumResult twoColumResult:(NSString *)twoColumResult threeColumResult:(NSString *)threeColumResult;
+
+@end
+
+@interface KNPickerView : UIView
+
+/**
+ 第一列数据，要是只有一列，则只传oneContents
+ */
+@property (nonatomic , strong) NSArray *oneContents;
+/**
+ 第二列数据，要是只有二列，则要传oneContents和twoContents
+ */
+@property (nonatomic , strong) NSArray *twoContents;
+/**
+ 第三列数据，要是有三列，则要传oneContents和twoContents和threeContents
+ */
+@property (nonatomic , strong) NSArray *threeContents;
+
+@property (nonatomic , weak) id <KNPickerViewDelegate> delegate;
+
+/**
+ 取消按钮
+ */
+@property (nonatomic , weak) UIButton *cancelButton;
+
+/**
+ 确定按钮
+ */
+@property (nonatomic , weak) UIButton *sureButton;
+/**
+ 显示视图
+ */
+- (void)show;
+
+
+@end
---
+++ KNPickerViewCarthage/KNPickerViewCarthage.h
@@ -0,0 +1,19 @@
+//
+//  KNPickerViewCarthage.h
+//  KNPickerViewCarthage
+//
+//  Created by 何凯楠 on 2017/5/22.
+//  Copyright © 2017年 HeXiaoBa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KNPickerViewCarthage.
+FOUNDATION_EXPORT double KNPickerViewCarthageVersionNumber;
+
+//! Project version string for KNPickerViewCarthage.
+FOUNDATION_EXPORT const unsigned char KNPickerViewCarthageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KNPickerViewCarthage/PublicHeader.h>
+
+
